75fbde1f02d22abd3e152ab54fb0aa3fb0920bca
Quan Gan <coin2028@hotmail.com>
2018-11-05 00:37:19 -0500

    downloading metadata

--- download.sh
+++ download.sh
@@ -7,9 +7,23 @@ ENDMONTH=$4
 
 DIR=data
 
+download () {
+	DEST=$1
+	URL=$2
+	if [[ ! -e $DEST ]]; then
+		wget -O $DEST $URL
+	else
+		echo Found $DEST
+	fi
+}
+
 if [[ ! -e $DIR ]]; then
 	mkdir -p $DIR
 fi
+
+download "$DIR/taxi_zones.zip" https://s3.amazonaws.com/nyc-tlc/misc/taxi_zones.zip
+unzip $DIR/taxi_zones.zip
+download "$DIR/taxi_zone_lookup.csv" https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv
 
 for (( i=$STARTYEAR; i<=$ENDYEAR; ++i )); do
 	if [ $i -eq $STARTYEAR ]; then
@@ -27,11 +41,6 @@ for (( i=$STARTYEAR; i<=$ENDYEAR; ++i )); do
 		MONTH=`printf %04d-%02d $i $j`
 		DEST="$DIR/$MONTH.csv"
 		URL="https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_$MONTH.csv"
-
-		if [[ ! -e $DEST ]]; then
-			wget -O $DEST $URL
-		else
-			echo Found $DEST
-		fi
+		download $DEST $URL
 	done
 done
