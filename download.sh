4c1eceab851c5bb5138b35f2bfeae9253f7e4a69
Gan Quan <coin2028@hotmail.com>
2018-11-08 17:02:14 -0500

    connection graph

--- download.sh
+++ download.sh
@@ -34,6 +34,10 @@ fi
 
 download_and_unzip "$DIR/taxi_zones.zip" https://s3.amazonaws.com/nyc-tlc/misc/taxi_zones.zip
 download_and_unzip "$DIR/nyc_street_maps.zip" "https://data.cityofnewyork.us/api/geospatial/exjm-f27b?method=export&format=Shapefile"
+mv $DIR/geo_export*.dbf $DIR/geo_export.dbf
+mv $DIR/geo_export*.prj $DIR/geo_export.prj
+mv $DIR/geo_export*.shp $DIR/geo_export.shp
+mv $DIR/geo_export*.shx $DIR/geo_export.shx
 download "$DIR/Centerline.pdf" "https://data.cityofnewyork.us/api/views/exjm-f27b/files/cba8af99-6cd5-49fd-9019-b4a6c2d9dff7?download=true&filename=Centerline.pdf"
 download "$DIR/taxi_zone_lookup.csv" https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv
 
